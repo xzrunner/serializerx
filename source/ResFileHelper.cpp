0ce418801376f3e45d87bbe8dd177ac22b6c74b0
zz <zhuguang@ejoy.com>
2018-06-14 23:31:04 +0800

    [ADDED] load xml

--- source/ResFileHelper.cpp
+++ source/ResFileHelper.cpp
@@ -9,11 +9,12 @@ ResFileType ResFileHelper::Type(const std::string& filepath)
 {
 	ResFileType type = RES_FILE_UNKNOWN;
 	auto ext = boost::filesystem::extension(filepath);
+	std::transform(ext.begin(), ext.end(), ext.begin(), tolower);
 	if (ext == ".png" || ext == ".jpg" || ext == ".bmp" || ext == ".ppm" || ext == ".pvr" || ext == ".pkm" || ext == ".dds") {
 		type = RES_FILE_IMAGE;
 	} else if (ext == ".json") {
 		type = RES_FILE_JSON;
-	} else if (ext == ".param" || ext == ".obj" || ext == ".m3d" || ext == ".X") {
+	} else if (ext == ".param" || ext == ".obj" || ext == ".m3d" || ext == ".x" || ext == ".xml") {
 		type = RES_FILE_MODEL;
 	} else if (ext == ".bin") {
 		type = RES_FILE_BIN;
